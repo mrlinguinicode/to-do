d7526a3d49ac45b7f9121b26f08f621c365e075a
mrlinguinicode <mrlinguini@protonmail.com>
2022-02-07 19:22:36 -0500

    beginnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
 function App() {
   return (
     <div>
-      <h4>HELLO there</h4>
+      <div className="title">
+        <h1>TODO LIST</h1>
+      </div>
+
+      <div className="todo-input">
+        <TextField placeholder="Enter text" multiline />
+        <div>
+          <Button className="add" variant="contained" size="large">
+            Add
+          </Button>
+        </div>
+      </div>
+
+      <div className="todo-items">
+        <div className="item">
+          <p>hello</p>
+        </div>
+        <div className="item">
+          <p>waddup</p>
+        </div>
+        <div className="item">
+          <p>hello</p>
+        </div>
+        <div className="item">
+          <p>waddup</p>
+        </div>
+        <div className="item">
+          <p>hello</p>
+        </div>
+        <div className="item">
+          <p>waddup</p>
+        </div>
+      </div>
     </div>
   );
 }
